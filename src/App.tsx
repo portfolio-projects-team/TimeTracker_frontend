b91d7f28ec8fce6486cf4f53a87baa980bd99625
Shimen <afsharshimen@gmail.com>
2023-12-03 14:08:41 +0000

    login start

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Center, ChakraProvider, Container, Heading } from "@chakra-ui/react";
 import { apptheme } from "./theme";
+import Login from './Login';
 
 function App() {
   return (
     <>
       <ChakraProvider theme={apptheme}>
+
         <Container>
           <Center h="100vh">
             <Heading>App coming soon!</Heading>
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Login from "./Login.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
