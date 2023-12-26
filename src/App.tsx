62148aa9fa482d6eaac880a0fa0fd7056f3f8f2b
Shimen <afsharshimen@gmail.com>
2023-12-26 13:11:04 +0000

    task form compo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Center, ChakraProvider, Container, Heading } from "@chakra-ui/react";
 import { apptheme } from "./theme";
 
+
+
 function App() {
   return (
     <>
       <ChakraProvider theme={apptheme}>
+
         <Container>
           <Center h="100vh">
             <Heading>App coming soon!</Heading>
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import TaskForm from "./TaskForm.tsx";
+
+
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+
+  {
+    path: "/TaskForm",
+    element: <TaskForm />,
+  },
+
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
