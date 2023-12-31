cf7892a4e4d1ccb9270dd23c1c5677fa4644902d
levilu800b <levilu800b@gmail.com>
2023-12-31 16:44:19 +0000

    ready for review

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -21,7 +21,7 @@ const CloseIcon = () => (
   <svg width="24" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
     <title>Close</title>
     <path
-      fill="white"
+      fill="black"
       d="M9.00023 7.58599L13.9502 2.63599L15.3642 4.04999L10.4142 8.99999L15.3642 13.95L13.9502 15.364L9.00023 10.414L4.05023 15.364L2.63623 13.95L7.58623 8.99999L2.63623 4.04999L4.05023 2.63599L9.00023 7.58599Z"
     />
   </svg>
@@ -32,7 +32,7 @@ const MenuIcon = () => (
     width="24px"
     viewBox="0 0 20 20"
     xmlns="http://www.w3.org/2000/svg"
-    fill="white"
+    fill="black"
   >
     <title>Menu</title>
     <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
@@ -73,14 +73,13 @@ const MenuLinks: React.FC<MenuLinksProps> = ({ isOpen }) => {
       <Stack
         spacing={8}
         align="center"
-        justify={['center', 'space-between', 'flex-end', 'flex-end']}
+        justify={['center', 'space-between']}
         direction={['column', 'row', 'row', 'row']}
         pt={[4, 4, 0, 0]}
       >
         <MenuItem to="/">Home</MenuItem>
-        <MenuItem to="/how">How It works </MenuItem>
-        <MenuItem to="/features">Features </MenuItem>
-        <MenuItem to="/pricing">Pricing </MenuItem>
+        <MenuItem to="/dashboard">Dashboard</MenuItem>
+        <MenuItem to="/signin">Sign In</MenuItem>
         <MenuItem to="/signup" isLast>
           <Button>Create Account</Button>
         </MenuItem>
@@ -104,6 +103,11 @@ const NavBarContainer: React.FC<NavBarContainerProps> = ({
       w="100%"
       mb={8}
       p={8}
+      zIndex={20}
+      pos="fixed"
+      top="0"
+      left="0"
+      overflow="auto"
       {...props}
     >
       {children}
