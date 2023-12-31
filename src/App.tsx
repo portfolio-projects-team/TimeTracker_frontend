d0c50460ffebda60c31b84aa2c5b3b460044aa72
levilu800b <levilu800b@gmail.com>
2023-12-31 17:07:41 +0000

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import { Center, ChakraProvider, Container} from "@chakra-ui/react";
-import { apptheme } from "./theme";
-import NavBar from "./components/Navbar/navbar";
-
+import { ChakraProvider, Flex, Box } from '@chakra-ui/react';
+import { apptheme } from './theme';
+import NavBar from './components/Navbar/navbar';
+import { Footer } from './components/Footer/footer';
 
 function App() {
   return (
     <ChakraProvider theme={apptheme}>
-      <Container>
-        <Center>
+      <Flex
+        minHeight="100vh"
+        flexDirection="column"
+        justifyContent="space-between"
+      >
+        <Box>
           <NavBar />
-        </Center>
-      </Container>
+        </Box>
+        <Box flex="1">{/* Your main content goes here */}</Box>
+        <Box>
+          <Footer />
+        </Box>
+      </Flex>
     </ChakraProvider>
   );
 }
