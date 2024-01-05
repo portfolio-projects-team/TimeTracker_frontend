17fd52e34f0d7195c3340f09d2ff4e5595967586
Shimen <afsharshimen@gmail.com>
2024-01-05 21:31:45 +0000

    dashbord

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Center, ChakraProvider, Container } from '@chakra-ui/react';
 import { apptheme } from './theme';
 import { SignUp } from './components/SignUp/signUp'; // Replace with your other component import
 import { SignIn } from './components/SignIn/signIn'; // Replace with your other component import
+import  Dashboard  from "./components/Dashboard/Dashboard";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
               <Route path="/" />
               <Route path="/signup" element={<SignUp />} />
               <Route path="/signin" element={<SignIn />} />
+              <Route path="/dashboard" element={<Dashboard />} /> 
             </Routes>
           </Router>
         </Center>
